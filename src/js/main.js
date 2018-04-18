56f444684382ddddba611523a764ac559245b22a
Jason Snider <root@jasonsnider.com>
2018-04-18 11:55:20 -0500

    Refactor

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,63 @@
-//Get the height and width of the main element.
-var mWidth = document.querySelector('main').offsetWidth;
-var mHeight = document.querySelector('main').offsetHeight;
+var draw = (function(){
 
-//Create the canvas
-var canvas = document.createElement('canvas');
-canvas.height = mHeight;
-canvas.width = mWidth;
-document.querySelector('main').appendChild(canvas);
+    //Get the height and width of the main element.
+    var mWidth = document.querySelector('main').offsetWidth;
+    var mHeight = document.querySelector('main').offsetHeight;
 
-//create the context
-var ctx = canvas.getContext('2d');
+    //Create the canvas
+    var canvas = document.createElement('canvas');
 
-//draw some sample rectangles
-ctx.fillStyle = 'rgb(200,0,0)';
-ctx.fillRect(10,10,55,50);
+    //create the context
+    var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-ctx.fillRect(30,30,55,50);
-
-canvas.addEventListener('mousemove', function(evt){
-
-    //calculate the x,y cords.
+    //Create the initial bounding rectangle
     var rect = canvas.getBoundingClientRect();
-    var x = evt.clientX - rect.left;
-    var y = evt.clientY - rect.top;
 
-    //write the cords back to the UI
-    document.getElementById('trackX').innerHTML = 'X: ' + x;
-    document.getElementById('trackY').innerHTML = 'Y: ' + y;
+    //current x,y position
+    var x = 0;
+    var y = 0;
+
+    return {
+
+        //Set the x,y cords based on current event data
+        setXY: function(evt){
+            x = (evt.clientX - rect.left) - canvas.offsetLeft;
+            y = (evt.clientY - rect.top) - canvas.offsetTop;
+        },
+
+        //Write the x,y cords based on current event data
+        writeXY: function(){
+            document.getElementById('trackX').innerHTML = 'X: ' + x;
+            document.getElementById('trackY').innerHTML = 'Y: ' + y;
+        },
+
+        //Access the canvas
+        getCanvas: function(){
+            return canvas;
+        },
+
+        //Draw a rectangle
+        drawRect: function(){
+            ctx.fillStyle = 'rgb(200, 0, 0)';
+            ctx.fillRect(10, 10, 55, 50);
+        },
+
+        //Initialize the object, this must be called before anything else
+        init: function(){
+            canvas.width = mWidth;
+            canvas.height = mHeight;
+            document.querySelector('main').appendChild(canvas);
+        }
+    }
+
+})();
+
+//Initialize draw
+draw.init();
+
+draw.drawRect();
+
+draw.getCanvas().addEventListener('mousemove', function(evt){
+    draw.setXY(evt);
+    draw.writeXY();
 });
